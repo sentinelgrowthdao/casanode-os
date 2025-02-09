1fe2650b1c1a2619a8ee4ab10c19166e91cbede4
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-02-09 22:07:49 +0100

    refactor: Simplify casanode installation by removing obsolete git clone and checkout steps

--- casanode/00-run.sh
+++ casanode/00-run.sh
@@ -17,66 +17,13 @@ apt-get update
 apt-get install -y nodejs
 EOF
 
-# Git clone
-echo "Cloning repository..."
-git clone https://github.com/sentinelgrowthdao/casanode-ble "${ROOTFS_DIR}/opt/casanode/sources/"
-
-# Fetch specific commit hash
-git -C "${ROOTFS_DIR}/opt/casanode/sources/" fetch origin <commit-hash>
-
-# Check if the commit hash exists in the repository
-if git -C "${ROOTFS_DIR}/opt/casanode/sources/" rev-parse --verify "<commit-hash>" >/dev/null 2>&1; then
-	# Checkout the specified commit hash
-	git -C "${ROOTFS_DIR}/opt/casanode/sources/" checkout "<commit-hash>" || error_exit "Failed to checkout specified commit hash."
-fi
-
-# Move app directory
-mv "${ROOTFS_DIR}/opt/casanode/sources/app/" "${ROOTFS_DIR}/opt/casanode/app/"
-rm -rf "${ROOTFS_DIR}/opt/casanode/sources/"
-
-# Install dependencies and build
-echo "Install npm dependencies and build the application..."
+# Install casanode
+echo "Installing casanode..."
 on_chroot << EOF
-npm install --prefix /opt/casanode/app
-npm run build --prefix /opt/casanode/app
+echo "deb [trusted=yes] https://sentinelgrowthdao.github.io/casanode-ble/ stable main" > /etc/apt/sources.list.d/casanode.list
+apt-get update
+apt-get install -y casanode=<deb-version>
 EOF
-
-# Install casanode startup.sh
-echo "Installing casanode startup.sh..."
-install -m 644 files/startup.sh "${ROOTFS_DIR}/opt/casanode/startup.sh"
-chmod +x "${ROOTFS_DIR}/opt/casanode/startup.sh"
-
-# Create casanode user
-echo "Creating casanode user..."
-on_chroot << EOF
-adduser --disabled-password --gecos "" --home /opt/casanode --uid 150 casanode
-chown -R casanode: /opt/casanode
-usermod -aG sudo,adm,docker casanode
-sed -i '/%sudo\s\+ALL=(ALL:ALL) ALL/a casanode ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
-EOF
-
-# Create casanode configuration
-echo "Creating casanode configuration..."
-install -m 644 files/casanode.conf "${ROOTFS_DIR}/etc/casanode.conf"
-on_chroot << EOF
-chown -R casanode: /etc/casanode.conf
-chmod 600 /etc/casanode.conf
-EOF
-
-# Create log directory
-on_chroot << EOF
-mkdir /var/log/casanode/
-chown -R casanode: /var/log/casanode/
-chmod 755 /var/log/casanode
-EOF
-
-# Create systemd service
-echo "Creating systemd service..."
-install -m 644 files/casanode.service "${ROOTFS_DIR}/etc/systemd/system/casanode.service"
-install -m 644 files/casanode-startup.service "${ROOTFS_DIR}/etc/systemd/system/casanode-startup.service"
-echo "Enabling systemd service..."
-ln -sv "/lib/systemd/system/casanode.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/casanode.service"
-ln -sv "/lib/systemd/system/casanode-startup.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/casanode-startup.service"
 
 # Create logrotate configuration
 echo "Creating logrotate configuration..."
